51a86aa726eb84ec3db2ffe92f3ad84ab6685b01
Handher0 <handher0@naver.com>
2024-08-01 00:48:58 +0900

    임시

--- applicants/models.py
+++ applicants/models.py
@@ -20,7 +20,7 @@ class Application(models.Model):
         ('interview_completed', 'Interview Completed'),
     ]
 
-    template = models.ForeignKey(ApplicationTemplate, on_delete=models.CASCADE)
+    template = models.ForeignKey(ApplicationTemplate, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=15)
     school = models.CharField(max_length=100)
@@ -28,7 +28,7 @@ class Application(models.Model):
     submission_date = models.DateTimeField(auto_now_add=True)
     interviewer = models.ForeignKey(Interviewer, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='submitted')
-
+    interview_date = models.DateTimeField(blank=True, null=True) 
 
 class Answer(models.Model):
     application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name='answers')
