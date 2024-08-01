1d1378c91f2a850896bdb8859d4c4de58c8128b8
yumin22 <22joomin@naver.com>
2024-08-02 01:55:19 +0900

    [feat] 템플릿 메인, create css 구현

--- template/admin.py
+++ template/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from template.models import ApplicationTemplate
 
 # Register your models here.
+admin.site.register(ApplicationTemplate)
--- template/forms.py
+++ template/forms.py
@@ -6,6 +6,15 @@ class ApplicationTemplateForm(forms.ModelForm):
         model = ApplicationTemplate
         fields = ['name' , 'description']
 
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': '템플릿 제목을 입력하세요.'
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': '(선택) 지원자들을 위한 지원서 작성 가이드를 입력해 주세요.'
+            }),
+        }
+
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
