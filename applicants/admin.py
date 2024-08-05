c1ddcb1e01c9f428a7a8eb96924aa34fffcf2e8c
Jihun Jeon <jihunee01@gmail.com>
2024-08-05 16:10:43 +0900

    자동 스케줄링 기능 추가

--- applicants/admin.py
+++ applicants/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Application, ApplicationTemplate, Question, Answer
+from .models import Application, ApplicationTemplate, Question, Answer, Possible_date_list
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.register(Application)
 admin.site.register(ApplicationTemplate)
 admin.site.register(Question)
 admin.site.register(Answer)
+admin.site.register(Possible_date_list)
--- applicants/urls.py
+++ applicants/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("document", views.document, name="document"),
     path('search_applicant/', views.search_applicant, name='search_applicant'),
     path("document/profile/<int:pk>", views.profile, name='profile'),
+    path("schedule", views.schedule, name='schedule'),
+    path("auto_schedule", views.auto_schedule, name='auto_schedule'),
 ]
