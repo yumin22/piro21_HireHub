177ac79677145284628bb421c1196ef3122092dd
yumin22 <22joomin@naver.com>
2024-08-09 09:28:49 +0900

    [ex] 녹음 기능 구현 시작

--- evaluations/views.py
+++ evaluations/views.py
@@ -9,7 +9,7 @@ from django.http import HttpResponseForbidden
 def create_evaluation(req, pk):
     application = Application.objects.get(id=pk)
     interviewer = Interviewer.objects.get(id = req.user.id)
-    template = EvaluationTemplate.objects.get(id=18)
+    template = EvaluationTemplate.objects.get(id=1)
 
     if not interviewer in application.interviewer.all():  # 배정된 면접관인지 확인
         return HttpResponseForbidden("배정된 면접관이 아닙니다.")
